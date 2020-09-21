fedd423013fd8069c528389f36a867704da13acc
pavan kumar tulasi <thulasipavankumar@gmail.com>
2020-09-21 19:45:23 +0200

    added a route for /

---
+++ src/main/java/org/example/SpringBoot/Humor.java
@@ -0,0 +1,41 @@
+package org.example.SpringBoot;
+
+import org.example.SpringBoot.WordCalculator.Sample;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+@RestController
+public class Humor {
+
+    @GetMapping("/whoami")
+    public String whomai() {
+        return "You're are my master Pavan Kumar Tulasi";
+    }
+
+    @GetMapping("/whoareyou")
+    public String whoareyou() {
+        return  getRandomValue()+"Iam running on"+getComputerName();
+    }
+    public String getRandomValue(){
+        String arr[] ={"Iam  Batman \n","Iam one punch man\n","Iam a bot but i would like to be called as JARVIS\n"};
+        return arr[(int )(Math.random() * 2 + 0)];
+    }
+    @GetMapping("/")
+    public Sample root(Sample sample) {
+        return  sample;
+    }
+    private static String getComputerName()
+    {
+        Map<String, String> env = System.getenv();
+        if (env.containsKey("COMPUTERNAME"))
+            return env.get("COMPUTERNAME");
+        else if (env.containsKey("HOSTNAME"))
+            return env.get("HOSTNAME");
+        else
+            return "Unknown Computer";
+    }
+
+
+}
---
+++ src/main/java/org/example/SpringBoot/WordCalculator/Sample.java
@@ -0,0 +1,14 @@
+package org.example.SpringBoot.WordCalculator;
+
+public class Sample {
+    private final String answer="The answer to universe is 42",
+    status = "alive";
+    private final boolean isAlive=true;
+    public Sample(){
+
+    }
+    public boolean getisAlive(){return this.isAlive;}
+    public String getAnswer(){return this.answer;}
+    public String getStatus(){return this.status;}
+
+}
